b4b59d4b0ddba329a4a78a049bbb07707f81333d
damienmarchandfr <damien-marchand@outlook.fr>
2021-05-15 10:38:52 +0200

    test other date real request

--- src/tests/request.test.ts
+++ src/tests/request.test.ts
@@ -3,12 +3,12 @@ import * as moment from "moment";
 
 test("Make a real request to website.", async () => {
   const client = new BonjourMadameClient();
-  const date = moment("20210510T000000+0200").toDate();
+  const date = moment("20210421T000000+0200").toDate();
   const data = await client.getAtDate(date);
 
   expect(data).toStrictEqual({
-    title: "Madame “Princesse” Lundi.",
-    imgUrl: "https://i2.wp.com/bonjourmadame.fr/wp-content/uploads/2021/05/210510.jpg",
-    pageUrl: "http://www.bonjourmadame.fr/2021/05/10",
+    title: 'Madame Mercredi, fracture définitive et énigmatique.',
+    imgUrl: 'https://i0.wp.com/bonjourmadame.fr/wp-content/uploads/2021/04/210421.jpg',
+    pageUrl: 'http://www.bonjourmadame.fr/2021/04/21'
   });
 });
